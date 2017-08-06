67f7c1c840e3b0682c34b9ad36488751f26ea177
Ryan HAN <ryanhan@altitudelabs.com>
2017-08-06 22:19:57 +0800

    set up server

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports ={
+	port: 5000,
+	api: {
+		host: 'api.sandbox.paypal.com',
+		port: '',
+		paypal: {
+			client_id: 'EBWKjlELKMYqRNQ6sYvFo64FtaRLRR5BdHEESmha49TM',
+			client_secret: 'EO422dn3gQLgDbuwqTjzrFgFtaRLRR5BdHEESmha49TM'
+		},
+		braintree: {
+			merchant_id: '',
+			public_key: '',
+			private_key: ''
+		}
+	}
+};
